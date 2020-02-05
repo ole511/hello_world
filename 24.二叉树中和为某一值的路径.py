980407803d6117c4a3227b49087cea4cf1aaf267
ole511 <39465012+ole511@users.noreply.github.com>
2020-02-05 15:27:12 +0800

    Create 24.二叉树中和为某一值的路径.py

---
+++ 24.二叉树中和为某一值的路径.py
@@ -0,0 +1,34 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+'''
+二叉树递归实现前序遍历，用一个list保存路径上的值，遇到叶子节点求list里的和
+如果和等于期望值，把结果保存下来，最终输出所有结果
+注意：list为可变变量，每次保存的时候用切片实现拷贝
+'''
+class Solution:
+    # 返回二维列表，内部每个列表表示找到的路径
+    def FindPath(self, root, expectNumber):
+        # write code here
+        if not root:
+            return []
+        path=[]
+        res=[]
+        def find(root):
+            path.append(root.val)
+            if not root.left and not root.right:
+                if sum(path) == expectNumber:
+                    res.append(path[:])
+                path.pop()
+            else:
+                if root.left:
+                    find(root.left)
+                if root.right:
+                    find(root.right)
+                path.pop()
+            
+        find(root)
+        return res
