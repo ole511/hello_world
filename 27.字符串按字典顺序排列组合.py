6af037e5fc4e06ce0484086e585f7740ca258ca7
ole511 <39465012+ole511@users.noreply.github.com>
2020-02-09 22:07:17 +0800

    Update 27.字符串按字典顺序排列组合.py

--- 27.字符串按字典顺序排列组合.py
+++ 27.字符串按字典顺序排列组合.py
@@ -1,53 +1,74 @@
-# -*- coding:utf-8 -*-
+# -*- coding:utf-8 -*- 字典序排序法
 class Solution:
     def Permutation(self, ss):
         # write code here
-        if not ss:
+        if ss=='':
             return []
         s=list(ss)
         s.sort()
         l=len(s)
         strlist=[ss]
+        k,j=0,0  #为什么要初始化才能过？？！
         while True:
-            for i in range(l-2,-1,-1):
-                if s[i]<s[i+1]:
+            for k in range(l-2,-1,-1):
+                if s[k]<s[k+1]:
                     break
             for j in range(l-1,-1,-1):
-                if s[j]>s[i]:
+                if s[j]>s[k]:
                     break
-            if i==0 and j==0:
+            if k==0 and j==0:
                 break
-            s[i],s[j]=s[j],s[i]
-            s[i+1:]=reversed(s[i+1:])
+            s[k],s[j]=s[j],s[k]
+            s[k+1:]=reversed(s[k+1:])
             strlist.append(''.join(s))
+        strlist=sorted(strlist)
         return strlist
-
-
-
-
-
-
-
-
-# -*- coding:utf-8 -*-
+    
+    
+    
+    
+    # -*- coding:utf-8 -*- python高效递归
 class Solution:
     def Permutation(self, ss):
         # write code here
         if not ss:
             return []
+        if len(ss)==1:
+            return [ss]
+        res=[]
+        for i in range(len(ss)):
+            for j in self.Permutation(ss[:i]+ss[i+1:]):
+                res.append(ss[i]+j)
+        return sorted(set(res))
+
+
+
+
+
+
+
+
+# -*- coding:utf-8 -*-  两两交换法
+class Solution:
+    def Permutation(self, ss):
+        # write code here
+        if ss=='':
+            return []
         s=list(ss)
         s.sort()
         end=len(s)
-        res=[ss]
-        res.append(self.permu(s,0,end))
+        res=[]
+        self.permu(s,0,end,res)
+        # res=list(set(res))  用来删除序列中重复的元素
+        res=sorted(res)
         return res
     
-    def permu(self,s,position,end):
+    def permu(self,s,position,end,res):
         if position==end:
-            return '',join(s)
+            res.append(''.join(s))
         else:
             for i in range(position,end):
                 if i == position or s[i]!=s[position]:
                     s[i],s[position]=s[position],s[i]
-                    self.permu(s,position+1,end)
+                    self.permu(s,position+1,end,res)
                     s[i],s[position]=s[position],s[i]
