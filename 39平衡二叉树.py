66ca390e08045d550396ab5dc926e405211f51ba
ole511 <39465012+ole511@users.noreply.github.com>
2020-02-21 10:10:27 +0800

    Create 39平衡二叉树.py

---
+++ 39平衡二叉树.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-先序遍历计算左右子树的深度
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def IsBalanced_Solution(self, pRoot):
+        # write code here
+        if not pRoot:return True  #空树也是平衡二叉树
+        return self.depth(pRoot)!=-1
+        
+    def depth(self,pRoot):
+        if not pRoot: return 0
+        left=self.depth(pRoot.left)
+        if left==-1: return -1  #左子树不平衡直接退出
+        right=self.depth(pRoot.right)
+        if right==-1: return -1
+        if abs(left-right)<=1: return max(left,right)+1   #当左右子树的深度之差不超过1，返回子树的深度
+        else:return -1
+        
+        
+        
