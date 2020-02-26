f36c2dd444376b4e6eca1ffe4d926468e7635632
ole511 <39465012+ole511@users.noreply.github.com>
2020-02-26 11:08:11 +0800

    Update 44翻转单词顺序列.py

--- 44翻转单词顺序列.py
+++ 44翻转单词顺序列.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 -*-
+# -*- coding:utf-8 -*- 将字符串转换成序列再调整的方法
 class Solution:
     def ReverseSentence(self, s):
         # write code here
@@ -9,6 +9,7 @@ class Solution:
             temp[i],temp[-i-1]=temp[-i-1],temp[i]
         return ' '.join(temp)
     
-
-
-
+'''
+python不支持对源字符串直接进行修改。例如s[:3]="123"   s="123"+s[3:]都不能通过
+所以我就没用先把字符串整体反转，再局部反转的方法。
+'''
